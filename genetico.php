547a96d77f8778e95a5c402b19e7fcd56660ff30
Rafael Barboza <barboza.cardoso@gmail.com>
2011-08-10 16:52:38 -0300

    commit para o github

--- genetico.php
+++ genetico.php
@@ -4,6 +4,12 @@
 
 	$population = 200;
 
+	$ages = 1000;
+
+	$parents = 120;
+
+	$mutants = 40;
+
 	$gens = array();
 
 	$fitness = array();
@@ -22,9 +28,69 @@
 	}
 
 
+/*
+
+	for($x=0;$x<$ages,$x++) {
+		$fitness = calFitness($chrom);
+
+		sort($fitness);
+		$fitness = array_slice($fitness,0,$parents);
+
+		$sons = genSons($fitness,$chrom);
+
+		$sons = change($sons,$mutants);
+
+		$chrom = $sons+parents+$chrom;
+	}
+	$fitness = calFitness($chrom);
+	sort $fitness;
+	echo $fitness[0];
 
 
+	}
+*/
 
 	echo '<pre>';
-	print_r($chrom);
+	print_r($class_array);
 	echo '</pre>';
+
+
+	//quanto menor melhor
+	function calFitness($chrom){
+		foreach($chrom as $key=>$value) {
+			$fitness[$k] = calFitnessTeachers($value);
+			$fitness[$k] += callFitnessWeekClasses($value);	
+		}
+	}
+
+	function callFitnessWeekClasses($chrom) {
+		$chrom = explode_array($chrom);
+		for($x=0;$x<5;$x++) {
+			$valor = getValor($chrom[$x]);
+			if($valor < $maior || $maior != null)
+				$maior = $valor;
+			if($valor > $menor || $menor != null)
+				$menor = $valor;	
+		}
+	}
+
+	function getValor($chrom) {
+		$valor = 0;
+		foreach($chrom as $key => $value) {
+			$valor += $class_array[$value][3]; 
+		}
+		return $valor;
+	}
+
+
+	function callFitnessWeekClasses($chrom) {
+		$value = 0;
+
+		$chrom = explode_array($chrom);
+		foreach($chrom as $key=>$value) {
+			$arr = gerTeachers($value);
+			$uniqueArr = array_unique($arr);
+			$value += $arr - $uniqueArr;
+		}
+		return $value;
+	}	
